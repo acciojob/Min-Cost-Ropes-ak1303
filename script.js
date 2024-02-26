553c73d7980659bacd8ffbc84723b1ea1de9f55b
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-26 18:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function mincost(arr)
 // return the min cost
   let sum=0;
 	while(arr.length>1){
-		let [minIndex1,minIndex2]=arr.reduce((acc,val,idx)=> val<acc[0]?[idx,acc[0]]:(val<acc[1]?[acc[0],idx]:acc),[0,1]);
-		let currSum=arr[minIndex1]+arr[minIndex2]
-		sum+=currSum;
-		arr.splice(minIndex1,1,currSum);
-		arr.splice(minIndex2,1);
+		arr.sort();
+		let a = arr.shift();
+		let b = arr.shift();
+		sum+=a+b;
+		arr.push(a+b);
 	}
 	return sum;
 }
